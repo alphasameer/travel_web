4dade9e7dbf107284039815543966e66334b681b
alphasameer <sameerkhan40697@gmail.com>
2024-06-15 17:54:17 +0530

    initalized the git for remote branch

---
+++ script.js
@@ -0,0 +1,10 @@
+// respoinse menu
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile-menu");
+  });
+});
